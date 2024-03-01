ca4fb748153351601ff5261388b6915a8cdab97f
Igor Mashkov <imashkov@vtb.ru>
2024-03-01 23:45:49 +0300

    fix after review

--- src/main/java/ru/job4j/array/SimpleStringEncoder.java
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -3,29 +3,24 @@ package ru.job4j.array;
 public class SimpleStringEncoder {
     public static String encode(String input) {
         String result = "";
+        char symbol = input.charAt(0);
         int counter = 1;
-        if (input.length() == 1) {
-            return input;
-        }
-        for (int i = 0; i < input.length(); i++) {
-            if ((i + 1) == input.length()) {
-                if (counter == 1) {
-                    result = result + input.charAt(i);
-                } else {
-                    result = result + input.charAt(i) + counter;
-                }
-                break;
-            }
-            if (input.charAt(i) == input.charAt(i + 1)) {
-                counter = counter + 1;
+        for (int i = 1; i < input.length(); i++) {
+            char current = input.charAt(i);
+            if (current == symbol) {
+                counter++;
             } else {
-                if (counter == 1) {
-                    result = result + input.charAt(i);
-                } else {
-                    result = result + input.charAt(i) + counter;
+                result += symbol;
+                if (counter > 1) {
+                    result += counter;
                 }
+                symbol = current;
                 counter = 1;
             }
+        }
+        result += symbol;
+        if (counter > 1) {
+            result += counter;
         }
         return result;
     }
