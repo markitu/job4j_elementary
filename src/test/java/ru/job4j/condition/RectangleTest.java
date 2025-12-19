aeeb4b4c81a75d7e7d5823801c75020a7b8f6cf4
markitu <40546844+markitu@users.noreply.github.com>
2025-12-19 23:50:52 +0300

    Тестирование Класса Вычисления Диагонали Прямоугольника [#505281]

--- src/test/java/ru/job4j/condition/RectangleTest.java
+++ src/test/java/ru/job4j/condition/RectangleTest.java
@@ -33,17 +33,13 @@ class RectangleTest {
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
 
-
     @Test
     void whenLength1Point5AndWidth2Point5ThenCorrectDiagonal() {
         double length = 1.5;
         double width = 2.5;
-        double expected = Math.sqrt(Math.pow(length, 2)+ Math.pow(width, 2));
-
+        double expected = Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
         double output = Rectangle.diagonal(length, width);
-
         assertThat(output).isEqualTo(expected, withPrecision(0.01));
     }
-
 
 }
