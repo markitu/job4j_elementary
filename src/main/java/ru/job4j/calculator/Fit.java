c97bb05f3d744d675792c32ddc295f4ce3907e20
markitu <40546844+markitu@users.noreply.github.com>
2025-12-19 16:10:44 +0300

    2. Тесты для задачи "идеальный вес" [#505260]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return (height - 110) * 1.15;
+    }
+
+}
