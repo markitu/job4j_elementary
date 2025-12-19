09fe5784c727fb51c9d59da7fe7ad16def28ddcb
markitu <40546844+markitu@users.noreply.github.com>
2025-12-19 16:05:08 +0300

    Тестирование Класса Калькулятора Температуры Хранения [#505279]

---
+++ src/main/java/ru/job4j/calculator/TemperatureFit.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculator;
+
+public class TemperatureFit {
+
+    public static double idealTemperatureForFruits(int temperature) {
+        return temperature - 2.0;
+    }
+
+    public static double idealTemperatureForMeat(int temperature) {
+        return temperature - 5.0;
+    }
+
+}
