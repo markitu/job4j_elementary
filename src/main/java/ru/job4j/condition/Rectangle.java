8ef8151cf36e09b460a19db7cfadabf82686510e
markitu <40546844+markitu@users.noreply.github.com>
2025-12-19 23:48:34 +0300

    Тестирование Класса Вычисления Диагонали Прямоугольника [#505281]

---
+++ src/main/java/ru/job4j/condition/Rectangle.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class Rectangle {
+
+    public static double diagonal(double length, double width) {
+        return Math.sqrt(Math.pow(length, 2) + Math.pow(width, 2));
+    }
+
+}
