5e5258084786384184fd561201b6c06ec75408c6
markitu <40546844+markitu@users.noreply.github.com>
2026-01-08 11:46:45 +0300

    *Optimize prime check and add tests
    Простое число (оператор прерывания break) [#505214 #556367]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,14 +3,12 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public static boolean check(int number) {
-        boolean prime = number > 1;
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i * i <= number; i++) {
             if (number % i == 0) {
-                prime = false;
-                break;
+                return false;
             }
         }
-        return prime;
+        return number > 1;
     }
 
 }
--- src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
+++ src/test/java/ru/job4j/loop/CheckPrimeNumberTest.java
@@ -54,4 +54,18 @@ class CheckPrimeNumberTest {
         boolean result = CheckPrimeNumber.check(number);
         assertThat(result).isFalse();
     }
+
+    @Test
+    void when10ThenFalse() {
+        int number = 10;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void when2ThenTrue() {
+        int number = 2;
+        boolean result = CheckPrimeNumber.check(number);
+        assertThat(result).isTrue();
+    }
 }
