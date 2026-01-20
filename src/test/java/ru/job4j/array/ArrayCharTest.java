c36dc88cdd17d0b664618c9051c2bacfaf4e9313
markitu <40546844+markitu@users.noreply.github.com>
2026-01-20 14:39:47 +0300

    Слово начинается с ... [#505233]

---
+++ src/test/java/ru/job4j/array/ArrayCharTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArrayCharTest {
+
+    @Test
+    public void whenStartWithPrefixThenTrue() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'e'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenNotStartWithPrefixThenFalse() {
+        char[] word = {'H', 'e', 'l', 'l', 'o'};
+        char[] prefix = {'H', 'i'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenEqualThenTrue() {
+        char[] word = {'P', 'r', 'i', 'v', 'e', 't'};
+        char[] prefix = {'P', 'r', 'i', 'v', 'e', 't'};
+        boolean result = ArrayChar.startsWith(word, prefix);
+        assertThat(result).isTrue();
+    }
+}
